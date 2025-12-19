65079708022de95ccda59d44dcf1c5a07f70dc27
MarcinZietek <Administrator@WINSRV2019BD>
2025-12-19 17:39:02 +0100

    Added Nagrody Entity

---
+++ Nagrody.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Nagrody]
+(
+	idNagrody int primary key identity(1,1),
+	nazwaNagrody char(200),
+	typNagrody char(100), -- główna, specjalna, honorowa
+	ekwiwalentPieniezny decimal(16,2) null
+)
